c54ee502f0e19cd75d0d7800fba58c593e401360
Ashish Kadam <ashishkadam83@gmail.com>
2021-10-04 18:34:26 +0530

    feat: For logging add package bunyan

--- controllers/list.controller.js
+++ controllers/list.controller.js
@@ -1,4 +1,7 @@
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({name: "myapp"});
 module.exports.list = (req, res, next) => {
+    log.error("List api");
     res.json({
         value: 'Response from list'
     });

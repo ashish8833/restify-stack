ed3a1c1fb34daf8ce983b35a94b5a7a73840dbfa
Ashish Kadam <ashishkadam83@gmail.com>
2021-10-05 19:46:40 +0530

    feat: slonik.

--- controllers/list.controller.js
+++ controllers/list.controller.js
@@ -1,10 +1,11 @@
 var bunyan = require('bunyan');
 var log = bunyan.createLogger({name: "myapp"});
-const slonik = require('slonik');
+const { sql } = require('slonik');
 const pool = require("../models");
 module.exports.list = async (req, res, next) => {
     if(req.body){
-        let list = await pool.many(slonik.sql`SELECT * FROM ${slonik.sql.identifier(['users'])}`);
+        // let list = await pool.many(sql`SELECT * FROM users LEFT JOIN address ON users.id = address.user_id where user.id = 1`);
+        let list = await pool.many(sql`SELECT * FROM ${sql.identifier(['users'])}`);
         return res.json(list);
     }else{
         return res.json({
